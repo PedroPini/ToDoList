9caae022e11e6267d29faa2b452108ad776ee3a6
PedroPini <augustopini@gmail.com>
2020-02-16 14:20:22 +0800

    Adding TODo project

---
+++ src/environments/environment.ts
@@ -0,0 +1,18 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD5h8Kdt3O2TCOMN-qEn__9wMJgqCQKFGQ",
+    authDomain: "todolistapp-86e6a.firebaseapp.com",
+    databaseURL: "https://todolistapp-86e6a.firebaseio.com",
+    projectId: "todolistapp-86e6a",
+    storageBucket: "todolistapp-86e6a.appspot.com",
+    messagingSenderId: "905002815414",
+    appId: "1:905002815414:web:4d614ccc4483c2db51bb17",
+    measurementId: "G-5YSEYZTJ3R"
+  }
+};
